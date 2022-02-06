35292e6e4add894b852305bea146f3bcc3a63696
agruezo <agruezo@gmail.com>
2022-02-06 17:56:58 -0500

    made code readable

--- Week1/Extra Practice/Cafe_Java/CafeJava.java
+++ Week1/Extra Practice/Cafe_Java/CafeJava.java
@@ -32,8 +32,9 @@ public class CafeJava {
     	// ** Your customer interaction print statements will go here ** //
         System.out.println(customer1 + pendingMessage);
 
-        
-
+        // Noah ordered a cappucino
+        // Check the status of the order
+        // If it is ready tell him total
         if (isReadyOrder4) {
             System.out.println(customer4 + readyMessage);
             System.out.println(displayTotalMessage + cappucinoPrice);
@@ -42,6 +43,8 @@ public class CafeJava {
             System.out.println(customer4 + pendingMessage);
         }
 
+        // Sam ordered two lattes and would like to know total
+        // Let him know order status
         System.out.println(displayTotalMessage + lattePrice*2);
         if (isReadyOrder2) {
             isReadyOrder2 = false;
@@ -50,7 +53,9 @@ public class CafeJava {
         else {
             System.out.println(customer2 + readyMessage);
         }
-
+        
+        // Jimmy was charged for coffee but ordered a latte
+        // Let him know how much he owes
         System.out.println(customer3 + displayTotalMessage + (lattePrice-dripPrice));
 
 
